6487f28a8d1c4da11e7a1acb4670868c35eab4dc
Rob Tallguy <tallguyking@gmail.com>
2020-01-26 00:11:10 -0800

    fixed the grid not covering the whole page

--- gui.py
+++ gui.py
@@ -2,15 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-root.geometry('{}x{}'.format(800, 600))
+root.geometry('{}x{}'.format(600, 600))
 
 root.title("Search")
 
 def noNothing():
     print("woot, nothing")
 
-#layout for main containers
-
+#Layout for menu
 menu = Menu(root)
 root.config(menu=menu)
 
@@ -20,41 +19,37 @@ subMenu.add_command(label='A* Search', command=noNothing)
 subMenu.add_command(label='Breadth-First Search', command=noNothing)
 menu.add_command(label='Start', command=noNothing)
 
-
-search_frame = Frame(root, bg='pink')
-search_frame.pack(fill=BOTH)
-
-
-square = Canvas(search_frame)
-isPressed = True
+#functions for clicking a square
+square = Canvas(root, width=600, height=600)
 
 def click(event):
-    isPressed = True
     if square.find_withtag(CURRENT):
         square.itemconfig(CURRENT, fill='yellow')
+    print(event)
+
+
 
 def unclick(event):
-    isPressed = False
-# square.create_rectangle(0, 0, 25, 25, fill='blue', outline='red', activefill='black')
-                        #0, 0, 0+25, 0+25
-# square.pack()
-# square.create_rectangle(25, 0, 50, 25, fill='blue', outline='red', activefill='black')
-                        #25, 0, +25, +25
-# square.pack()
-# square.create_rectangle(50, 0, 75, 25, fill='blue', outline='red', activefill='black')
-# square.pack()
-root.update_idletasks()
-for y in range(800):
+    print(event)
+
+def coords(event):
+    if(event.x%25 == 0 or event.y%25 == 0):
+        print(event)
+        # print(square.gettags(square.find_withtag(CURRENT)))
+        # square.itemconfig(ACTIVE, fill='yellow')
+        # print(event.widget.configure(background='yellow'))
+        
+
+#printing grid of squares
+
+for y in range(600):
     for x in range(600):
         if x%25 == 0 and y%25 == 0:
-            square.create_rectangle(x, y, x+25, y+25, fill='blue', outline='red')
-square.pack(fill=BOTH)  
+            square.create_rectangle(x, y, x+25, y+25, fill='black', outline='white', tags='{},{}'.format(x,y))
+square.pack(expand=True)  
 
-square.bind("<ButtonPress-1>", click)
+# square.bind("<Button-1>", click)
 square.bind("<ButtonRelease-1>", unclick)
+square.bind("<B1-Motion>", coords)
 
-# b = Canvas(top_frame, width=80, height=80, bg="pink", cursor="circle")
-# b.pack()
-
-# top_frame.pack()
 root.mainloop()
