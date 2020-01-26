ba94813711bfee0e52527dc4985b86951660f04f
Rob Tallguy <tallguyking@gmail.com>
2020-01-25 22:49:19 -0800

    added top menu, need to figure out grid of squares

--- gui.py
+++ gui.py
@@ -6,28 +6,52 @@ root.geometry('{}x{}'.format(800, 600))
 
 root.title("Search")
 
+def noNothing():
+    print("woot, nothing")
+
 #layout for main containers
-top_frame = Frame(root, bg='blue', width=800, height=50)
-search_frame = Frame(root, bg='pink', width=800, height=550)
 
-top_frame.grid(row=0)
-search_frame.grid(row=1)
+menu = Menu(root)
+root.config(menu=menu)
 
-#the list of search types
-search_list = [
-    'A* search',
-    'Breadth-first search'
-]
-variable_of_search = StringVar(root)
-variable_of_search.set(search_list[0])
+subMenu = Menu(menu)
+menu.add_cascade(label='Search type', menu=subMenu)
+subMenu.add_command(label='A* Search', command=noNothing)
+subMenu.add_command(label='Breadth-First Search', command=noNothing)
+menu.add_command(label='Start', command=noNothing)
 
-#widgets for top frame
-search_label = Label(top_frame, text='Type') #search_type.current() for the current index
-search_type = OptionMenu(top_frame, variable_of_search, *search_list)
-# prompt_for_start = 
 
-search_label.grid(row=0, column=0)
-search_type.grid(row=0, column=1)
+search_frame = Frame(root, bg='pink')
+search_frame.pack(fill=BOTH)
+
+
+square = Canvas(search_frame)
+isPressed = True
+
+def click(event):
+    isPressed = True
+    if square.find_withtag(CURRENT):
+        square.itemconfig(CURRENT, fill='yellow')
+
+def unclick(event):
+    isPressed = False
+# square.create_rectangle(0, 0, 25, 25, fill='blue', outline='red', activefill='black')
+                        #0, 0, 0+25, 0+25
+# square.pack()
+# square.create_rectangle(25, 0, 50, 25, fill='blue', outline='red', activefill='black')
+                        #25, 0, +25, +25
+# square.pack()
+# square.create_rectangle(50, 0, 75, 25, fill='blue', outline='red', activefill='black')
+# square.pack()
+root.update_idletasks()
+for y in range(800):
+    for x in range(600):
+        if x%25 == 0 and y%25 == 0:
+            square.create_rectangle(x, y, x+25, y+25, fill='blue', outline='red')
+square.pack(fill=BOTH)  
+
+square.bind("<ButtonPress-1>", click)
+square.bind("<ButtonRelease-1>", unclick)
 
 # b = Canvas(top_frame, width=80, height=80, bg="pink", cursor="circle")
 # b.pack()
