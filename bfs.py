8faa6bf0fe2ce9b99315b117b8992d57b682661b
Rob Tallguy <tallguyking@gmail.com>
2020-01-28 22:35:17 -0800

    added comments to the function

--- bfs.py
+++ bfs.py
@@ -23,41 +23,57 @@ class Bfs:
         self.next_node = 0
 
     def explore_neightbours(self, x, y):
+        #loop through possible neighbours, up, side, down, side
         for i in range(4):
             yy = y + self.dy[i]
             xx = x + self.dx[i]
 
+            #if the neighbour is off the grid, skip
             if yy < 0 or xx < 0: continue
             if yy >= 10 or xx >= 10: continue
 
+            #if the neighbour is already visited, skip
             if self.visited[xx][yy]: continue
 
+            #add the new neighbour to the list of next neighbours to explore
             self.xqueue.append(xx)
             self.yqueue.append(yy)
+            #mark that current neighbour as visited
             self.visited[xx][yy] = True
+            #unsure of this one, but keep track of that neighbour x y
             self.prev[self.next_node] = (xx, yy)
             # self.prev[self.next_node] = (xx, yy)
+            #this is marking the square blue for visual purposes
             if self.grid[xx][yy] != self.finish_point:
                 self.square.itemconfig(self.square.find_closest((xx * self.square_size),  (yy * self.square_size)), fill='blue')
+            #increase the neighbour count
             self.nodes_in_next_layer += 1
+            #increment the recording neighbours index
             self.next_node += 1
 
     def solve(self):
         #row is y, col is x
+        #mark the first point as being visited
         self.visited[self.starting_point[0]][self.starting_point[1]] = True
-        # print(f'{self.grid[self.xqueue.popleft()][self.yqueue.popleft()]}, {self.finish_point}')
         
+        #while there is still nodes to visit
         while len(list(self.xqueue)) > 0:
+            #create two queues, one for x and y
             x = self.xqueue.popleft()
             y = self.yqueue.popleft()
+            #if the current x y is the goal, break
             if self.grid[x][y] == self.finish_point:
                 self.reached_end = True
                 break
             self.explore_neightbours(x, y)
+            #decrease the nodes in the layer to 0 on first loop
             self.nodes_left_in_layer -= 1
             if self.nodes_left_in_layer == 0:
+                #change layers to be newly found neighbours
                 self.nodes_left_in_layer = self.nodes_in_next_layer
+                #put neighbours back to 0 for next loop
                 self.nodes_in_next_layer = 0
+                #increase the amount of steps needed for shortest path
                 self.move_count += 1
         if self.reached_end:
             tmp = [h for h in self.prev if h]
